7ad11110475baeef1a0f6e9f4ff4918cca63e7dc
yurlis <yuri.lisovski@gmail.com>
2024-12-10 01:04:41 +0200

    Swagger - correct https 5

--- src/main/java/yurlis/carassistantapp/config/OpenApi30Config.java
+++ src/main/java/yurlis/carassistantapp/config/OpenApi30Config.java
@@ -4,10 +4,15 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "API", version = "v1"))
+        //@OpenAPIDefinition(info = @Info(title = "API", version = "v1"))
+@OpenAPIDefinition(
+        info = @Info(title = "API", version = "v1"),
+        servers = @Server(url = "https://car-assistant-app-production.up.railway.app/api", description = "Production API server")
+)
 @SecurityScheme(
         name = "bearerAuth",
         type = SecuritySchemeType.HTTP,
